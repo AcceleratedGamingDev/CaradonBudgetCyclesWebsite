c269dd6b0cd8bc55a88dde988d2131492cdd02a8
MinerScottyDev <30982491+MinerScottyDev@users.noreply.github.com>
2017-09-29 20:27:34 +0100

    Created "index.php" file for homepage of website.

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Home - Caradon Budget Cycles</title>
+    <link rel="stylesheet" type="text/css" href="assets/css/caradonbudgetcycles.css" />
+  </head>
+  <body>
+    <div align="center">
+      <h3>Caradon Budget Cycles</h3>
+    </div> 
+  </body>
+</html>
